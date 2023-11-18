80f85e4107a8eca344d76fce2e9d4568c21c9ddc
Walter white <86451921+MRbhiman@users.noreply.github.com>
2023-11-18 12:16:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQTheFileDonorBot
+  git clone https://github.com/MRbhiman/Jesse_Pinkman.git /JessePinkman
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /JessePinkmanBot
 fi
-cd /DQTheFileDonorBot
+cd /JessePinkmanBot
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting Jesse_Pinkman...."
 python3 bot.py
