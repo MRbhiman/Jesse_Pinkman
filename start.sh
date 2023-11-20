def520b26dbeab1a783ba608a75fbf6ae27c6b69
Walter white <86451921+MRbhiman@users.noreply.github.com>
2023-11-20 19:29:44 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MRbhiman/Jesse_Pinkman.git /DQTheFileDonorBot
+  git clone https://github.com/MRbhiman/Jesse_Pinkman.git /Jesse_Pinkman
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /Jesse_Pinkman
 fi
 cd /DQTheFileDonorBot
 pip3 install -U -r requirements.txt
