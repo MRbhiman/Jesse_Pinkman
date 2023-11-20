2d7cecce25555ad3eae118190d139fc9abb85c1c
Walter white <86451921+MRbhiman@users.noreply.github.com>
2023-11-20 14:59:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/MRbhiman/Jesse_Pinkman.git /DQTheFileDonorBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /Jesse_Pinkman
 fi
 cd /DQTheFileDonorBot
 pip3 install -U -r requirements.txt
-echo "Starting Jesse-pinkman...."
+echo "Starting Jesse_Pinkman...."
 python3 bot.py
